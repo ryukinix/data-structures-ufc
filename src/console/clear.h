75ef61e657c85d42e93f56f946cc4b70edb8aff4
Manoel Vilela <manoel_vilela@engineer.com>
2017-08-29 20:05:13 -0300

    Adiciona clear.h como uma função para limpar o terminal
    Isso é feito de uma maneira portátil entre Windows e Linux.

---
+++ src/console/clear.h
@@ -0,0 +1,30 @@
+/**
+ * ============================================================================
+ *
+ *                      Copyright 2017 Manoel Vilela
+ *
+ *         Author: Manoel Vilela
+ *        Contact: manoel_vilela@engineer.com
+ *   Organization: UFC
+ *
+ * ============================================================================
+ */
+
+#ifndef CLEAR_H
+#define CLEAR_H
+
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <stdio.h>
+#define clrscr() printf("\e[1;1H\e[2J")
+#endif
+
+/**
+ * @brief if on windows use the horrible conion.h, otherwise use ANSI escapes
+ */
+void clear() {
+    clrscr();
+}
+
+#endif
