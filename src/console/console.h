8168d819ccdfe663952ba2da74b190c8058100be
Manoel Vilela <manoel_vilela@engineer.com>
2017-08-29 20:48:37 -0300

    Define src/console/console.h para importar todos headers de console
    Isso se torna uma biblioteca útil para trabalho com consoles.
    Além disso, já é mudado em src/circle/main.c seu uso.

---
+++ src/console/console.h
@@ -0,0 +1,19 @@
+/**
+ * ============================================================================
+ *
+ *                      Copyright 2017 Manoel Vilela
+ *
+ *         Author: Manoel Vilela
+ *        Contact: manoel_vilela@engineer.com
+ *   Organization: UFC
+ *
+ * ============================================================================
+ */
+
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+#include "pause.h"
+#include "clear.h"
+
+#endif
