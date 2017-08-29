faf2be736ea74324a4866f342a1d0da270036b3e
Manoel Vilela <manoel_vilela@engineer.com>
2017-08-29 13:54:44 -0300

    Adiciona student.h em src/vector-struct
    Preciso implementar todos os exemplos de alocação das estruturas:
    
    - src/vector-struct/pointer-vector-struct-dynamic.c
    - src/vector-struct/pointer-vector-struct-static.c
    - src/vector-struct/vector-struct-dynamic.c
    - src/vector-struct/vector-struct-static.c

---
+++ src/vector-struct/student.h
@@ -0,0 +1,19 @@
+/**
+ * ============================================================================
+ *
+ *                      Copyright 2017 Manoel Vilela
+ *
+ *         Author: Manoel Vilela
+ *        Contact: manoel_vilela@engineer.com
+ *   Organization: UFC
+ *
+ * ============================================================================
+ */
+
+/**
+ * @brief just a struct to represent a Student
+ */
+typedef struct {
+    char name[81]; /**< name of the student */
+    float ira; /**< student individual academic coefficient */
+} Student;
