d9f388670ad81eec10e48565022467a0a6693c8f
Manoel Vilela <manoel_vilela@engineer.com>
2017-09-04 21:56:51 -0300

    Adiciona código de exemplo para Vetor de Estruturas Dinâmico

---
+++ src/vector-struct/vector-struct-dynamic.c
@@ -0,0 +1,85 @@
+/**
+ * ============================================================================
+ *
+ *                      Copyright 2017 Manoel Vilela
+ *
+ *         Author: Manoel Vilela
+ *        Contact: manoel_vilela@engineer.com
+ *   Organization: UFC
+ *
+ * ============================================================================
+ */
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include "student.h"
+
+#define STUDENT_EMPTY -1
+
+void init(Student *students, int n) {
+    for(int i = 0; i < n; i++) {
+        students[i].ira = STUDENT_EMPTY;
+    }
+}
+
+void print_student(int i, Student *students, int n) {
+    assert(i >= 0 || i < n);
+    if (students[i].ira != STUDENT_EMPTY) {
+        student_print(students + i);
+    }
+}
+
+void print_students(Student *students, int n) {
+    for(int i = 0; i < n; i++) {
+        print_student(i, students, n);
+    }
+}
+
+void update_student(int i, Student *students, int n) {
+    assert(i >= 0 || i < n);
+    float ira = -1;
+    printf("Name: ");
+    scanf(" %[^\n]", students[i].name);
+    while (1) {
+        printf("Ira: ");
+        scanf("%f", &ira);
+        if (ira < 0 || ira > 100) {
+            printf("[ErrorValue] Ira value must be on interval [0, 100].\n");
+        } else {
+            break;
+        }
+    }
+    students[i].ira = ira;
+}
+
+void update_students(Student *students, int n) {
+    for(int i = 0; i < n; i++) {
+        update_student(i, students, n);
+    }
+}
+
+int main(void) {
+    Student *students = NULL;
+    int n; // number of students
+    printf("Number of Students: ");
+    scanf("%d", &n);
+
+    // memory alloc
+    students = (Student *) malloc(sizeof(Student)*n);
+    if (students == NULL) {
+        printf("Not enough memory available.");
+        exit(1);
+    }
+
+    // main program
+    init(students, n);
+    update_students(students, n);
+    print_students(students, n);
+
+    // free memory
+    free(students);
+    students = NULL;
+
+    return 0;
+}
