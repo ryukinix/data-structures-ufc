643dcf78e4c056d114bf69c4c2727d895dbe8ea5
Manoel Vilela <manoel_vilela@engineer.com>
2017-09-04 21:55:32 -0300

    Adiciona novos procedimentos no cabeçalho src/vector-struct/student.h

--- src/vector-struct/student.h
+++ src/vector-struct/student.h
@@ -10,10 +10,38 @@
  * ============================================================================
  */
 
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STUDENT_MAX_NAME 81
+
 /**
  * @brief just a struct to represent a Student
  */
 typedef struct {
-    char name[81]; /**< name of the student */
+    char name[STUDENT_MAX_NAME]; /**< name of the student */
     float ira; /**< student individual academic coefficient */
 } Student;
+
+
+void char_repeat(int n, char c) {
+    for(int i = 0; i < n; i++) {
+        printf("%c", c);
+    }
+    printf("\n");
+}
+
+void student_print(Student *student) {
+    char_repeat(STUDENT_MAX_NAME, '=');
+    printf("Name: %s\n", student->name);
+    printf("IRA: %.3f\n", student->ira);
+}
+
+Student* student_new() {
+    return (Student *) malloc(sizeof(Student));
+}
+
+#endif
