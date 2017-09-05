f709e0d0b154062379bf27ffa3cb0730928b3d4a
Manoel Vilela <manoel_vilela@engineer.com>
2017-09-05 02:11:53 -0300

    Adiciona Tipo Abstrato de Dado: Point

---
+++ src/tad/point/point.c
@@ -0,0 +1,61 @@
+/**
+ * ============================================================================
+ *
+ *                      Copyright 2017 Manoel Vilela
+ *
+ *         Author: Manoel Vilela
+ *        Contact: manoel_vilela@engineer.com
+ *   Organization: UFC
+ *
+ * ============================================================================
+ */
+
+#include <stdlib.h>
+#include <math.h>
+#include "point.h"
+
+struct point {
+    float x;
+    float y;
+};
+
+
+Point* point_create(float x, float y) {
+    Point* p = (Point *) malloc(sizeof(Point));
+    p->x = x;
+    p->y = y;
+    return p;
+}
+
+void point_free(Point *p) {
+    free(p);
+}
+
+
+void point_set(Point *p, float x, float y) {
+    p->x = x;
+    p->y = y;
+}
+
+
+void point_get(Point *p, float *x, float *y) {
+    *x = p->x;
+    *y = p->y;
+}
+
+float point_get_x(Point *p) {
+    return p->x;
+}
+
+float point_get_y(Point *p) {
+    return p->y;
+}
+
+void point_copy(Point *origin, Point *destination) {
+    destination = origin;
+}
+
+float point_distance(Point *px, Point *py) {
+    return sqrt(pow(px->x - py->x, 2)
+              + pow(px->y - py->y, 2));
+}
---
+++ src/tad/point/point.h
@@ -0,0 +1,66 @@
+/**
+ * ============================================================================
+ *
+ *                      Copyright 2017 Manoel Vilela
+ *
+ *         Author: Manoel Vilela
+ *        Contact: manoel_vilela@engineer.com
+ *   Organization: UFC
+ *
+ * ============================================================================
+ */
+
+#ifndef POINT_H
+#define POINT_H
+
+/**
+ * @brief struct Point as 2D space pointer
+ */
+typedef struct point Point;
+
+/**
+ * @brief create a new Point and set x an y
+ *
+ * @return the address of the allocated point
+ */
+Point* point_create(float x, float y);
+
+/**
+ * @brief free memory for the Point p
+ * @param p Point to be free
+ */
+void point_free(Point *p);
+
+/**
+ * @brief Set the values of p.x and p.y
+ */
+void point_set(Point *p, float x, float y);
+
+/**
+ * @brief Get the values of p.x and p.y through the pointers *x and *y
+ */
+void point_get(Point *p, float *x, float *y);
+
+/**
+ * @brief Get the x value
+ */
+float point_get_y(Point *p);
+
+/**
+ * @brief Get the y value
+ */
+float point_get_x(Point *p);
+
+/**
+ * @brief Copy point origin to destination
+ */
+void point_copy(Point *origin, Point *destination);
+
+/**
+ * @brief calculate the euclidean distance between two points
+ *
+ * @return the distance of *px and *py as a float number
+ */
+float point_distance(Point *px, Point *py);
+
+#endif
