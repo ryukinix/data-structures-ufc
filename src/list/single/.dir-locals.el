d1c7b60e8fe7565b84e0088677708a20a94db7d4
Manoel Vilela <manoel_vilela@engineer.com>
2017-09-12 22:59:42 -0300

    Simplifica arquivo .dir-locals.el

--- src/list/single/.dir-locals.el
+++ src/list/single/.dir-locals.el
@@ -2,6 +2,5 @@
 
 ;; this add `include' additional paths to flycheck (linter) and
 ;; company-mode (auto-completion) correctly fetch the headers of the project.
-((nil . ((eval . (let ((src-path (expand-file-name "../../")))
-                   (setq-local flycheck-clang-include-path (list src-path))
-                   (company-clang-set-prefix (list '-include src-path)))))))
+((nil . ((flycheck-clang-include-path . ("../../"))
+         (company-clang-arguments . ("-I../../")))))
