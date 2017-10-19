6e9e93d01cc37db3e162f2eade96beed3831a19a
Manoel Vilela <manoel_vilela@engineer.com>
2017-10-19 00:20:16 -0300

    [test] Generaliza testes para inserção e remoção com array
    Definido um array, é gerado N testes.
    
    Por enquanto são apenas testes visuais, não unit-tests. O ideal
    seria ter testes unitários, mas como testar se a árvore é igual
    ao esperado, estou simplificando as coisas aqui.

--- src/tree/bst/test.c
+++ src/tree/bst/test.c
@@ -12,21 +12,35 @@
 
 #include <stdio.h>
 #include "bst.h"
+#define N 5
 
 int main(void) {
     BSTree* t = bst_create();
-    t = bst_insert(t, 10);
-    t = bst_insert(t, 20);
-    t = bst_insert(t, 5);
-    t = bst_insert(t, 80);
-    t = bst_insert(t, 50);
-    bst_print(t);
+    int array[N] = {10, 20, 5, 80, 50};
+
+    // insertion
+    for(int i = 0; i < N; i++) {
+        printf("Insert: %d\n", array[i]);
+        t = bst_insert(t, array[i]);
+        bst_print(t);
+    }
+
+    // infix, posfix, prefix
+    printf("== Notations\n");
     printf("Infix: ");
     bst_infix(t);
     printf("Prefix: ");
     bst_prefix(t);
     printf("Posfix: ");
     bst_posfix(t);
+
+    // remove
+    for(int i = 0; i < N; i++) {
+        printf("Remove: %d\n", array[i]);
+        t = bst_remove(t, array[i]);
+        bst_print(t);
+    }
+
     bst_free(t);
     return 0;
 }
