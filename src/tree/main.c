abdd58c4f63df2d5e8c0b08782cceebbd24922dc
Manoel Vilela <manoel_vilela@engineer.com>
2017-10-22 11:41:17 -0300

    [tree] Adiciona programa iterativo pra exploração de BSTree
    É possível realizar as operações de:
    * print
    * insert
    * remove
    * infix
    * prefix
    * posfix
    
    Dessa maneira, junto com o mundo ascii-tree, fica bem intuitivo
    o que está acontecendo com a árvore. Será interessante usar esse
    mesmo programa e mudar para outras estruturas, como avl e rb.

---
+++ src/tree/main.c
@@ -0,0 +1,52 @@
+/**
+ * ================================================
+ *
+ *         Copyright 2017 Manoel Vilela
+ *
+ *         Author: Manoel Vilela
+ *        Contact: manoel_vilela@engineer.com
+ *   Organization: UFC
+ *
+ * ===============================================
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "bst/bst.h"
+#include "ascii-tree/ascii-tree.h"
+#include "console/console.h"
+
+/**
+ * @description
+ * A simple program to see what happens on BST
+ * interactively inserting and removing new elements
+ */
+
+int main(void) {
+    BSTree *root = bst_create();
+    int number;
+    char command[50];
+    printf("usage: [insert num | remove num | infix | posfix | prefix | print | clear]\n");
+    while (1) {
+        printf(">>> ");
+        scanf("%s", command);
+        if (strcmp(command, "insert") == 0) {
+            scanf(" %d", &number);
+            root = bst_insert(root, number);
+        } else if (strcmp(command, "remove") == 0) {
+            scanf(" %d", &number);
+            root = bst_remove(root, number);
+        } else if (strcmp(command, "infix") == 0) {
+            bst_infix(root);
+        } else if (strcmp(command, "posfix") == 0) {
+            bst_posfix(root);
+        } else if (strcmp(command, "prefix") == 0) {
+            bst_prefix(root);
+        } else if (strcmp(command, "print") == 0) {
+            print_ascii_tree(root);
+        } else if (strcmp(command, "clear") == 0) {
+            clear();
+        }
+    }
+
+}
