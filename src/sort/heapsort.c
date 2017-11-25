40858df1a888d90fc6bb4b8cab7258b16c7c6c65
Manoel Vilela <manoel_vilela@engineer.com>
2017-11-25 05:22:25 -0300

    [sort] Adiciona alguns comentários no algoritmo de heapsort

--- src/sort/heapsort.c
+++ src/sort/heapsort.c
@@ -17,17 +17,23 @@ void heapify(Type v[], int n, int i) {
     int l = 2*i + 1;
     int r = 2*(i + 1);
 
+    // verify if left node is greater
+    // than largest
     if (l < n && v[l] > v[largest]) {
         largest = l;
     }
 
+    // verify if the right node is greater
+    // than largest
     if (r < n && v[r] > v[largest]) {
         largest = r;
     }
 
+    // if current node is not the largest
+    // need swaps
     if (largest != i) {
-        swap(v+i, v+largest);
-        heapify(v, n, largest);
+        swap(v+i, v+largest); // current and largest
+        heapify(v, n, largest); // apply recursively
     }
 }
 
