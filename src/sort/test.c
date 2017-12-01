ad2215da907b1772d6bb7eebbc16cea1b9c163e8
Manoel Vilela <manoel_vilela@engineer.com>
2017-12-01 20:29:17 -0300

    [sort] deixa test.c mais robusto

--- src/sort/test.c
+++ src/sort/test.c
@@ -11,47 +11,43 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "sort.h"
 
 // HACK: macro rules
-#define TEST(ALGORITHM, V, S)                   \
+#define N 10
+#define TEST(ALGORITHM)                         \
+    int *v = random_vector(N);                  \
     printf("== Testing for %s: ", #ALGORITHM);  \
-    print_vector(V, S);                         \
-    ALGORITHM(V, S);                            \
+    print_vector(v, N);                         \
+    ALGORITHM(v, N);                            \
     printf("Finished: ");                       \
-    print_vector(V, S)
+    print_vector(v, N);                         \
+    assert(check_sorted(v, N));                 \
+    free(v)
 
 void test_bubblesort(void) {
-    Type v[] = {5,2,3,1,3,5};
-    int n = 6;
-    TEST(bubblesort, v, n);
+    TEST(bubblesort);
 }
 
 void test_insertionsort(void) {
-    Type v[] = {5,2,3,1,3,5};
-    int n = 6;
-    TEST(insertionsort, v, n);
+    TEST(insertionsort);
 
 }
 
 void test_quicksort(void) {
-    Type v[] = {5,2,3,1,3,5};
-    int n = 6;
-    TEST(quicksort, v, n);
+    TEST(quicksort);
 
 }
 
 void test_mergesort(void) {
-    Type v[] = {5,2,3,1,3,5};
-    int n = 6;
-    TEST(mergesort, v, n);
+    TEST(mergesort);
 
 }
 
 void test_heapsort(void) {
-    Type v[] = {5,2,3,1,3,5};
-    int n = 6;
-    TEST(heapsort, v, n);
+    TEST(heapsort);
 
 }
 
