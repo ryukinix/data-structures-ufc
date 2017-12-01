526d3abb5f8198b1f6c2a1804d5f39b842500b1e
Manoel Vilela <manoel_vilela@engineer.com>
2017-12-01 19:17:48 -0300

    [sort] Adiciona protótipo de solução main.c
    Esse arquivo conterá os procedimentos necessários para
    poder testar todos os algoritmos e salvar os dados em um csv.

---
+++ src/sort/main.c
@@ -0,0 +1,21 @@
+/**
+ * ================================================
+ *
+ *         Copyright 2017 Manoel Vilela
+ *
+ *         Author: Manoel Vilela
+ *        Contact: manoel_vilela@engineer.com
+ *   Organization: UFC
+ *
+ * ===============================================
+ */
+
+
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(void) {
+    return 0;
+}
